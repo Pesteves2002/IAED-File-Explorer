3ba06e4a749bc8e361ce22bb08be8a33bdeac720
Tomás Esteves <tomasesteves2002@gmail.com>
2021-05-11 14:40:05 +0100

    Add help and quit function

--- Makefile
+++ Makefile
@@ -1,17 +1,7 @@
-.SUFFIXES: .in .out .diff
-MAKEFLAGS += --no-print-directory # No entering and leaving messages
-OK="\e[1;32mtest $< PASSED\e[0m"
-KO="\e[1;31mtest $< FAILED\e[0m"
-EXE=../proj2
-
-all:: clean # run regression tests
-	@$(MAKE) $(MFLAGS) `ls *.in | sed -e "s/in/diff/"`
-
-valgrind:: clean
-	@$(MAKE) $(MFLAGS) EXE="valgrind $(EXE)" `ls *.in | sed -e "s/in/diff/"`
-
-.in.diff:
-	@-$(EXE) < $< | diff - $*.out > $@
-	@if [ `wc -l < $@` -eq 0 ]; then echo $(OK); else echo $(KO); fi;
-
+CC=gcc
+CFLAGS=-Wall -Wextra -Werror -ansi -pedantic -g
+proj2: *.c 
+proj2.zip: *.c 
+	zip proj2.zip *.c 
 clean::
+	rm -f proj1 a.out *.o core tests/*.diff proj1.zip
---
+++ proj2.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define HELP_CMD "help"
+#define QUIT_CMD "quit"
+#define SET_CMD "set"
+#define PRINT_CMD "print"
+#define FIND_CMD "find"
+#define LIST_CMD "list"
+#define SEARCH_CMD "search"
+#define DELETE_CMD "delete"
+
+#define HELP_ACTION "help: Imprime os comandos disponíveis."
+#define QUIT_ACTION "quit: Termina o programa"
+#define SET_ACTION "set: Adiciona ou modifica o valor a armazenar."
+#define PRINT_ACTION "print: Imprime todos os caminhos e valores."
+#define FIND_ACTION "find: Imprime o valor armazenado."
+#define LIST_ACTION "list: Lista todos os componentes de um caminho."
+#define SEARCH_ACTION "search: Procura o caminho dado um valor."
+#define DEL_ACTION "Apaga um caminho e todos os subcaminhos."
+
+#define NUM_CMD 8
+#define CMD_SZ 10
+
+void help_func();
+
+int main()
+{
+    char ler_input[CMD_SZ];
+
+    scanf("%s", ler_input);
+
+    while (strcmp(ler_input, QUIT_CMD))
+    {
+        if (!strcmp(ler_input, HELP_CMD))
+            help_func();
+
+        scanf("%s", ler_input);
+    }
+
+    return 0;
+}
+
+void help_func()
+{
+    char *list[NUM_CMD] = {
+        HELP_ACTION,
+        QUIT_ACTION,
+        SET_ACTION,
+        PRINT_ACTION,
+        FIND_ACTION,
+        LIST_ACTION,
+        SEARCH_ACTION,
+        DEL_ACTION,
+    };
+    int i;
+
+    for (i = 0; i < NUM_CMD; ++i)
+    {
+        printf("%s\n", list[i]);
+    }
+}
